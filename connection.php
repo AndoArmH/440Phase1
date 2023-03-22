6e20ccba7813202cd116d23fe11131b75bcabe23
= <=>
2023-03-22 13:03:11 -0700

    server test fix

--- connection.php
+++ connection.php
@@ -1,15 +1,15 @@
 <?php
 
-// $dbhost = "localhost";
-// $dbuser = "root";
-// $dbpass = "";
-// $dbname = "440";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "440";
 
-$dbhost = "frpro2.fcomet.com"; // cpanel Shared IP Address
-$dbuser = "auraxium_440group";
-$dbpass = "im696fz3OG2g";
-$dbname = "auraxium_440";
-//port is 3306
+// $dbhost = "frpro2.fcomet.com"; // cpanel Shared IP Address
+// $dbuser = "auraxium_440group";
+// $dbpass = "im696fz3OG2g";
+// $dbname = "auraxium_440";
+// //port is 3306
 
 if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
 	die("failed to connect");
