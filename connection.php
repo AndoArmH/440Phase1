38fd89075a7322ed0c71b323fe79c73951d7f914
= <=>
2023-03-22 13:00:29 -0700

    server test

--- connection.php
+++ connection.php
@@ -1,13 +1,19 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "440";
+// $dbhost = "localhost";
+// $dbuser = "root";
+// $dbpass = "";
+// $dbname = "440";
 
-//connect to database
-//if statement if connection fails
+$dbhost = "frpro2.fcomet.com"; // cpanel Shared IP Address
+$dbuser = "auraxium_440group";
+$dbpass = "im696fz3OG2g";
+$dbname = "auraxium_440";
+//port is 3306
 
 if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
 	die("failed to connect");
 }
+
+echo "Logged in"
+?>
